86af9bac479f48ad6a5a5da572cd25b34e671d93
Barry Wark <barrywark@gmail.com>
2013-01-25 09:51:33 -0500

    That didn't do it; trying explicit path

--- src/main/python/build_wrapper.sh
+++ src/main/python/build_wrapper.sh
@@ -3,7 +3,7 @@
 M2_REPO=$1
 VERSION=$2
 
-/usr/bin/env virtualenv --distribute wrapper.build.venv
+/usr/local/bin/virtualenv --distribute wrapper.build.venv
 source wrapper.build.venv/bin/activate
 
 pip install jcc
