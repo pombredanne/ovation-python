278f855215ed47def9b1b46e12f409ff2e3bc663
Barry Wark <barrywark@gmail.com>
2013-05-20 13:32:25 -0400

    Corrected test_maps

--- ovation/conversion.py
+++ ovation/conversion.py
@@ -1,13 +1,13 @@
 import collections
 
 import numpy as np
-import scipy as sp
 import quantities as pq
 
 from scipy.io import netcdf
 
-from ovation import *
-from ovation.core import *
+from ovation import JArray_double, JArray_int, JArray_object, Maps, Map, String, Object
+from ovation.core import NumericData
+
 
 def asarray(numeric_data):
     """Converts a numeric Ovation NumericData.Data to a Quantities (NumPy) array
@@ -28,14 +28,13 @@ def asarray(numeric_data):
     sampling_rates = numeric_data.getSamplingRates()
     sampling_rate_units = numeric_data.getSamplingRateUnits()
     dimension_labels = numeric_data.getDimensionLabels()
-    
 
     arr = np.reshape(np.array(JArray_double.cast_(numeric_data.getData())) * units, shape)
     if len(sampling_rate_units) == 1:
-        arr.sampling_rate = pq.Quantity(sampling_rates, sampling_rate_units[0]) 
+        arr.sampling_rate = pq.Quantity(sampling_rates, sampling_rate_units[0])
     else:
-        arr.sampling_rates = tuple(pq.Quantity(rate, unit) for (rate,unit) in zip(sampling_rates, sampling_rate_units))
-    
+        arr.sampling_rates = tuple(pq.Quantity(rate, unit) for (rate, unit) in zip(sampling_rates, sampling_rate_units))
+
     arr.labels = dimension_labels
     arr.name = numeric_data.getName()
 
@@ -55,67 +54,66 @@ def as_numeric_data(arr):
     numeric_data : ovation.NumericData
          `ovation.NumericData` instance copying the provided quantity
     """
-    
+
     dtype_to_java = {
-        np.float32 :    JArray_double,
-        np.float64 :    JArray_double,
-        np.int32 :      JArray_int
+        np.float32: JArray_double,
+        np.float64: JArray_double,
+        np.int32: JArray_int
     }
-    
+
     result = NumericData()
     if arr.ndim == 1:
         data = dtype_to_java[arr.dtype.type](np.asarray(arr).tolist())
-        result.addData(arr.name, 
-                        data,
-                        arr.dimensionality.unicode,
-                        float(arr.sampling_rate),
-                        arr.sampling_rate.dimensionality.unicode)
+        result.addData(arr.name,
+                       data,
+                       arr.dimensionality.unicode,
+                       float(arr.sampling_rate),
+                       arr.sampling_rate.dimensionality.unicode)
     elif arr.ndim == 2:
         data = JArray_object([dtype_to_java[arr.dtype.type](r) for r in np.asarray(arr).tolist()])
         try:
             dimension_labels = arr.labels
         except AttributeError:
             dimension_lablels = ["", ""]
-        
-        result.addData(arr.name,
-                        data,
-                        dimension_labels,
-                        arr.dimensionality.unicode,
-                        [float(arr.sampling_rate) for i in xrange(arr.ndim)],
-                        [arr.sampling_rate.dimensionality.unicode for i in xrange(arr.ndim)]
-                        )
-    else:
-        raise NotImplementedError("NumericData does not support rank-" + str(arr.ndim) +" arrays")
-        
-    return result
-    
-    
-def create_variable(ncf, data, dtype, dimensions, **attributes):
 
+        result.addData(arr.name,
+                       data,
+                       dimension_labels,
+                       arr.dimensionality.unicode,
+                       [float(arr.sampling_rate) for i in xrange(arr.ndim)],
+                       [arr.sampling_rate.dimensionality.unicode for i in xrange(arr.ndim)]
+        )
+    else:
+        raise NotImplementedError("NumericData does not support rank-" + str(arr.ndim) + " arrays")
+
+    return result
+
+
+def create_variable(ncf, data, dtype, dimensions, **attributes):
     shape = tuple([ncf.dimensions[dim] for dim in dimensions])
     shape_ = tuple([dim or 0 for dim in shape])  # replace None with 0 for numpy
 
-    type = dtype
-    typecode, size = type.char, type.itemsize
+    typecode, size = dtype.char, dtype.itemsize
     if (typecode, size) not in netcdf.REVERSE:
-        raise ValueError("NetCDF 3 does not support type %s" % type)
+        raise ValueError("NetCDF 3 does not support type {}".format(dtype))
 
-    data = empty(shape_, dtype=type.newbyteorder("B")) #convert to big endian always for NetCDF 3
+    data = np.empty(shape_, dtype=dtype.newbyteorder("B")) #convert to big endian always for NetCDF 3
     ncf.variables[name] = netcdf_variable(data, typecode, size, shape, dimensions, attributes=attributes)
     return ncf.variables[name]
 
 
 def to_map(d):
-    result = Maps.newHashMap().of_(String,Object)
-    for (k,v) in d.iteritems():
+    result = Maps.newHashMap().of_(String, Object)
+    for (k, v) in d.iteritems():
         if not isinstance(k, basestring):
             k = unicode(k)
         if isinstance(v, collections.Mapping):
             result.put(k, to_map(v))
         else:
             result.put(k, v)
-    
+
     return result
 
+
 def to_dict(m):
-    return Map.cast_(m)
+    return {key: m.get(key) for key in m.keySet()}
--- ovation/test_maps.py
+++ ovation/test_maps.py
@@ -1,57 +1,59 @@
 import collections
 from nose.tools import istest, assert_equals
 
-from ovation import *
+from ovation import Map, Maps, Integer
 from conversion import to_map, to_dict
+
 
 @istest
 def to_dict_should_convert_flat_map():
     m = Maps.newHashMap()
     m.put('key1', 'value1')
     m.put('key2', 2)
-    
+
     d = to_dict(m)
-    
+
     check_dict(d, m)
 
-    
+
 @istest
 def to_map_should_convert_flat_dict():
-    
-    d = {   'key1' : 'value1',
-            'key2' : 2,
-            3 : 'value3',
-            4 : 5 }
-            
-            
+    d = {'key1': 'value1',
+         'key2': 2,
+         3: 'value3',
+         4: 5}
+
     m = to_map(d)
-    
+
     check_dict(d, m)
 
 
 def check_dict(d, m):
-    for (k,v) in d.iteritems():
+    for (k, v) in d.iteritems():
         if not isinstance(k, basestring):
             k = unicode(k)
-        
+
         if isinstance(v, collections.Mapping):
             check_dict(v, Map.cast_(m.get(k)))
         else:
             if isinstance(v, basestring):
                 actual = unicode(m.get(k))
-            if isinstance(v, int):
+                assert_equals(v, actual)
+            elif isinstance(v, int):
                 actual = Integer.cast_(m.get(k)).intValue()
-                
-            assert_equals(v, actual)
-    
+                assert_equals(v, actual)
+            else:
+                actual = m.get(k)
+                assert v.equals(actual)
+
+
 @istest
 def to_map_should_convert_nested_dict():
-    d = {   'key1' : 'value1',
-            'nested' : {'key2' : 2,
-                        3 : 'value3',
-                        4 : 5 }}
-            
-            
+    d = {'key1': 'value1',
+         'nested': {'key2': 2,
+                    3: 'value3',
+                    4: 5}}
+
     m = to_map(d)
-            
+
     check_dict(d, m)   
