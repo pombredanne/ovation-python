446fa2213ff2198674f083bad7be765af86ba604
Barry Wark <barrywark@gmail.com>
2013-05-20 13:01:24 -0400

    API package corrected

--- ovation/__init__.py
+++ ovation/__init__.py
@@ -1,6 +1,6 @@
 import logging as pylogging
 
-from ovation-api import initVM, JArray
+from ovation_api import initVM, JArray
 import us.physion.ovation.api as api
 import us.physion.ovation.domain as core
 import us.physion.ovation.logging as logging
@@ -10,15 +10,13 @@ from com.google.common.collect import *
 from java.util import *
 from java.lang import *
 
-LOGGER = pylogging.getLogger('ovation')
-
-logFileHandler = pylogging.FileHandler('ovation-python')
-# formatter = pylogging.Formatter('%(asctime)s %(levelname)s %(message)s')
-# hdlr.setFormatter(formatter)
-LOGGER.addHandler(logFileHandler) 
-LOGGER.setLevel(pylogging.DEBUG)
-
 try:
     initVM()
 except Exception, e:
+    LOGGER = pylogging.getLogger('ovation')
+
+    logFileHandler = pylogging.FileHandler('ovation-python.log')
+    LOGGER.addHandler(logFileHandler) 
+    LOGGER.setLevel(pylogging.DEBUG)
+    
     LOGGER.error("Unable to initialize Java VM", e)
