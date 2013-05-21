89af4b3ef57cdb1f81a797fa9f8bf9366859cb66
Barry Wark <barrywark@gmail.com>
2013-05-21 17:29:21 -0400

    Moved nbgetpass to ovation.connection

--- ovation/connection.py
+++ ovation/connection.py
@@ -39,3 +39,63 @@ def connect(email, password=None, logging=True):
         pw = password
     
     return Ovation.connect(email, pw)
+
+
+def nbgetpass(prompt="Ovation.io Credentials", passwd='passwd', email='email'):
+    """getpass.getpass replacement for use in the IPython notebook.
+
+    Presents a JQuery dialog prompting for email and password, saving each to a workspace
+    variable.
+
+    NB: you should delete the corresponding workspace variables once they are no longer needed.
+
+    Arguments
+    ---------
+    prompt : string
+        Dialog title
+    passwd : string
+        Workspace variable containing the user-entered password
+    email : string
+        Workspace variable containing the user-entered email
+    """
+
+    from IPython.display import display, Javascript
+
+    display(Javascript("""
+var dialog = $('<div/>').append(
+    $('<input/>')
+    .attr('id', 'email')
+    .attr('name', 'email')
+    .attr('type', 'email')
+    .attr('value', '')
+    ).append(
+        $('<input/>')
+        .attr('id', 'password')
+        .attr('name', 'password')
+        .attr('type', 'password')
+        .attr('value', '')
+    );
+$(document).append(dialog);
+dialog.dialog({
+    resizable: false,
+    modal: true,
+    title: "%s",
+    closeText: '',
+    buttons : {
+        "Okay": function () {
+            IPython.notebook.kernel.execute(
+                "%s = '" + $("input#password").attr('value') + "'"
+            );
+            IPython.notebook.kernel.execute(
+                "%s = '" + $("input#email").attr('value') + "'"
+            );
+            $(this).dialog('close');
+            dialog.remove();
+        },
+        "Cancel": function () {
+            $(this).dialog('close');
+            dialog.remove();
+        }
+    }
+});
+""" % (prompt, passwd, email)), include=['application/javascript'])
