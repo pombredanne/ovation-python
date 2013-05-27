6696b093e2da16db1d647bb3b45b55f88e003e97
Barry Wark <barrywark@gmail.com>
2013-05-26 22:50:02 -0400

    Added URI, URL to wrapper

--- ovation/__init__.py
+++ ovation/__init__.py
@@ -3,6 +3,7 @@ from ovation_api import *
 from com.google.common.collect import *
 from java.util import *
 from java.lang import *
+from java.net import URI, URL
 from org.joda.time import *
 
 initVM()
