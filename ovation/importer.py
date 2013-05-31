4f2d8f80ea99cc15344c1362e6d1e19d9e199e7f
Barry Wark <barrywark@gmail.com>
2013-05-31 09:56:38 -0400

    Adds ovation.importer for template import_main

---
+++ ovation/importer.py
@@ -0,0 +1,113 @@
+# Copyright 2011, Physion Consulting LLC
+# -*- coding: utf-8 -*-
+
+import sys
+import argparse
+import getpass
+import logging
+
+from ovation.connection import connect
+
+def import_main(args=sys.argv,
+                name=__name__,
+                description=None,
+                file_ext=None,
+                parser_callback=None,
+                import_fn=None,
+                dsc=None):
+
+    """Basic importer main function for data importers
+
+    Caller may provide a `parser_callback` for adding additional `argparse` arguments.
+
+    The import function must be of the form::
+
+        import_fn(data_context,
+                  container_id=args.container,
+                  protocol_id=args.protocol,
+                  files=args.files,
+                  timezone=args.timezone,
+                  **args)
+
+    A minimal import __main__.py using `import_main`::
+
+        if __name__ == '__main__':
+            sys.exit(import_main(args=sys.argv,
+                                name=<importer name>,
+                                description=<importer description>,
+                                file_ext=<imported extension, e.g. csv>,
+                                import_fn=<importer function or callable))
+
+
+
+    Parameters
+    ----------
+    :parameter args : list
+        Commandline arguments
+    :parameter name : str
+        Importer name
+    :parameter description : str
+        Importer description
+    :parameter file_ext : str
+        File extension for files imported by this importer
+    :parameter parser_callback : callable
+        Parser callback for adding additional argparse arguemnts. Must be of the form callback(parser) -> parser
+    :parameter import_fn : callable
+        Import function
+    :parameter dsc : us.physion.ovation.api.DataStoreCoordinator
+        Optional DataStoreCoordinator, e.g. for testing.
+
+    Returns
+    -------
+    :return exit value
+    """
+
+
+    parser = argparse.ArgumentParser(prog=name,
+                                     description=description)
+
+    auth_group = parser.add_argument_group('authentication')
+    auth_group.add_argument('--user', help='ovation.io user email')
+    auth_group.add_argument('--password', help='ovation.io password')
+
+    parser.add_argument('files', metavar='f.{}'.format(file_ext), nargs='+', type=str)
+
+    parser.add_argument('--timezone',
+                        default=None,
+                        help='timezone name in which data was collected. Default = local time zone')
+
+    experiment_group = parser.add_argument_group('epoch container')
+    experiment_group.add_argument('--container', help='epoch container ID', required=True)
+    experiment_group.add_argument('--protocol', help='protocol ID', required=True)
+
+    if parser_callback:
+        parser = parser_callback(parser)
+
+    args = parser.parse_args(args=args)
+
+    logging.basicConfig(filename='import.log',level=logging.INFO)
+
+    if dsc is None:
+        if args.user is None:
+            args.user = raw_input('ovation.io user: ')
+
+        if args.password is None:
+            password = getpass.getpass(prompt='ovation.io password: ')
+        else:
+            password = args.password
+
+        dsc = connect(args.user, password)
+
+    ctx = dsc.getContext()
+
+    try:
+        return import_fn(ctx,
+                          container_id=args.container,
+                          protocol_id=args.protocol,
+                          files=args.files,
+                          timezone=args.timezone,
+                          **args)
+    except Exception:
+        logging.error('Unable to import {}'.format(args.files), exc_info=True)
+        return 1
+
