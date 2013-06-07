0eebd4ce6b5c760c635e660e2dbaf3e6758b9202
Barry Wark <barrywark@gmail.com>
2013-06-06 23:15:20 -0400

    Setup.py improvements

--- setup-api.py
+++ setup-api.py
@@ -9,23 +9,17 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
     
 VERSION = "2.0-SNAPSHOT"
-setup(name='ovation',
+setup(name='ovation-api',
       version=VERSION,
-      description='Ovation Python API',
+      description='Low-level Ovation API wrapper module',
       author='Physion',
       author_email='info@ovation.io',
       url='http://ovation.io',
       long_description=read('README.md'),
-      packages=['ovation', 'ovation.api', 'ovation.core', 'ovation.test', 'ovation.test.util'],
       classifiers=[
           "Development Status :: 4 - Beta",
           "Intended Audience :: Science/Research",
           "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
       ],
-      install_requires=[#"ovation-api >= {version}".format(version=VERSION),
-                        "scipy >= 0.12.0",
-                        "numpy >= 1.7.1",
-                        "pandas >= 0.11.0",
-                        "quantities >= 0.10.1",
-                        ],
+      install_requires=["jcc >= 2.16"]
 )
