b0ded1e08c57d00272952d44f613f3f0e652aed2
Barry Wark <barrywark@gmail.com>
2013-07-09 17:40:01 -0400

    mvn subprocess call check win32

--- dependencies.py
+++ dependencies.py
@@ -6,10 +6,10 @@ import os
 import os.path
 import shutil
 
-    
+
 def dependency_list(pom_path='pom.xml'):
     output_path = "maven.classpath.txt"
-    subprocess.check_call(["mvn", "-f", pom_path, "dependency:build-classpath", "-Dmdep.outputFile={0}".format(output_path)], shell=True)
+    subprocess.check_call(["mvn", "-f", pom_path, "dependency:build-classpath", "-Dmdep.outputFile={0}".format(output_path)], shell=(sys.platform=='win32'))
     
     with open(output_path) as f:
         classpath = f.read()
