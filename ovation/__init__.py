a6f7dc751f33b8486876a5500104969b95358e2e
Barry Wark <barrywark@gmail.com>
2013-07-15 11:19:23 -0400

    Adds Option to ovation namespace

--- ovation/__init__.py
+++ ovation/__init__.py
@@ -11,22 +11,24 @@ os.environ["CLASSPATH"] = os.path.pathsep.join(CLASSPATH)
 from jnius import autoclass, cast
 
 
-# Common references outside the us.physion.ovation namespace
+### Common references outside the us.physion.ovation namespace
+
+# java.
 Integer = autoclass("java.lang.Integer")
 Double = autoclass("java.lang.Double")
-
 Map = autoclass("java.util.Map")
-
 URL = autoclass("java.net.URL")
 URI = autoclass("java.net.URI")
-
 File = autoclass("java.io.File")
+TimeUnit = autoclass("java.util.concurrent.TimeUnit")
 
+# org.joda
 DateTime = autoclass("org.joda.time.DateTime")
 DateTimeZone = autoclass("org.joda.time.DateTimeZone")
 
+# com.google.common
 Sets = autoclass("com.google.common.collect.Sets")
 Maps = autoclass("com.google.common.collect.Maps")
+Optional = autoclass("com.google.common.base.Optional")
 
-TimeUnit = autoclass("java.util.concurrent.TimeUnit")
 
